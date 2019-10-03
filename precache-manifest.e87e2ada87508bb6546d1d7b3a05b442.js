7ceda710aa6d6e915767200b8f628fcb8bdfd001
Kim Anh Nguyen <kimanhou@gmail.com>
2019-10-03 18:25:30 +0200

    Updates

---
+++ precache-manifest.e87e2ada87508bb6546d1d7b3a05b442.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d8c9fae71e5d28e44ecc",
+    "url": "/calendar/static/js/runtime~main.d8c9fae7.js"
+  },
+  {
+    "revision": "88fa7ad6c627596f503a",
+    "url": "/calendar/static/js/main.88fa7ad6.chunk.js"
+  },
+  {
+    "revision": "361d3a3bcd0d89f78104",
+    "url": "/calendar/static/js/1.361d3a3b.chunk.js"
+  },
+  {
+    "revision": "88fa7ad6c627596f503a",
+    "url": "/calendar/static/css/main.7a3023f2.chunk.css"
+  },
+  {
+    "revision": "1eccb93b2c467f0bc432eb3ff8249941",
+    "url": "/calendar/index.html"
+  }
+];
